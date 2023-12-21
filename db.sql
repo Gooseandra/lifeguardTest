2b3bfe2ed99f6c705e742468ccf5b783cd7cd6b6
Gooseandra <43761062+Gooseandra@users.noreply.github.com>
2023-12-21 16:53:31 +0300

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,128 +1,211 @@
-create table "user"(
-    id serial primary key,
-    name varchar(20) not null,
-    surname varchar(30),
-    patronymic varchar(20),
-    password varchar(30) not null,
-    nick varchar(20),
-    phone varchar(10) not null,
-    vk varchar(50),
-    tg varchar(50),
-    email varchar(50),
-    private_data varchar(400)
+create table "user"
+(
+    id           serial
+        primary key,
+    name         varchar(20) not null,
+    surname      varchar(30),
+    patronymic   varchar(20),
+    password     varchar(30) not null,
+    nick         varchar(20),
+    phone        varchar(10) not null,
+    vk           varchar(50),
+    tg           varchar(50),
+    email        varchar(50),
+    private_data varchar(400),
+    apply        timestamp,
+    fired        timestamp
 );
 
-create table "event"(
-    id serial primary key,
-    name varchar(45),
-    discription varchar(100),
+alter table "user"
+    owner to postgres;
+
+create table event
+(
+    id                serial
+        primary key,
+    name              varchar(45),
+    discription       varchar(100),
     order_discription varchar(400),
-    start_date date,
-    start_time time,
-    end_date date,
-    ned_time time
+    start_date        date,
+    start_time        time,
+    end_date          date,
+    ned_time          time
 );
 
-create table "event_roaster"(
-    id serial primary key,
-    userId integer references "user" (id),
-    eventId integer references "event" (id),
-    leader bool
+alter table event
+    owner to postgres;
+
+create table event_roster
+(
+    id      integer default nextval('event_roaster_id_seq'::regclass) not null
+        constraint event_roaster_pkey
+            primary key,
+    userid  integer
+        constraint event_roaster_userid_fkey
+            references "user",
+    eventid integer
+        constraint event_roaster_eventid_fkey
+            references event,
+    leader  boolean
 );
 
-create table "crew_inventory"(
-    id serial primary key
-);
+alter table event_roster
+    owner to postgres;
 
-create table "inventory"(
-  id serial primary key,
-  name varchar(50),
-  value int,
-  crew_inventory integer references "crew_inventory"(id)
-);
-
-create table "car"(
-    id serial primary key,
-    name varchar(40),
-    comment varchar(400),
-    inventory integer references crew_inventory(id)
-);
-
-create table "day_crew"(
-    id serial primary key,
-    time_start time,
-    date_start date,
-    time_end time,
-    date_end time,
-    comment varchar(400)
-);
-
-create table "date_crew_roaster"(
-    "user" integer references "user"(id),
-    day_crew integer references day_crew(id),
-    leader bool
-);
-
-create table calls(
-    id serial primary key,
-    "desc" varchar(200),
-    summing_up varchar(200),
-    address varchar(45),
-    time time
-);
-
-create table crew_calls(
-    call_id integer references calls(id),
-    crew_id integer references day_crew(id)
-);
-
-create table ranks(
-  id serial primary key,
-  name varchar(20)
-);
-
-create table permission(
-    id serial primary key,
-    action bool
-);
-
-create table "rank_permissions"(
-    "permission" integer references "permission"("id"),
-    "rank" integer references "ranks"("id")
-);
-
-create table "codes"(
-    id serial primary key,
-    code varchar(10),
-    userId integer references "user"(id)
-);
-
-create table "event_inventory"(
-    eventID integer references event(id),
-    itemID integer references inventory(id)
-);
-
-create table "users_ranks"(
-    rankId integer references ranks(id),
-    userId integer references "user"(id)
-);
-
-create table "inventory"(
-    id serial primary key,
-    name varchar(50),
-    type integer references inventory
-);
-
-create table "inventory_instance"(
-    id serial primary key,
+create table inventory
+(
+    id          serial
+        primary key,
+    name        varchar(50),
+    type        varchar(20),
     description varchar(200),
-    number integer unique
-)
+    "uniqNum"   integer
+);
 
-select
-    case when exists (select true from inventory where "name" = 'sss')
-            then ''
-         else (INSERT INTO inventory_type values ('sss'))
-        end;
+alter table inventory
+    owner to postgres;
 
-insert into inventory values ('sss')
+create table crew_inventory
+(
+    id      serial
+        primary key,
+    item_id integer
+        references inventory
+);
+
+alter table crew_inventory
+    owner to postgres;
+
+create table car
+(
+    id        serial
+        primary key,
+    name      varchar(40),
+    comment   varchar(400),
+    inventory integer
+        references crew_inventory
+);
+
+alter table car
+    owner to postgres;
+
+create table day_crew
+(
+    id         serial
+        primary key,
+    comment    varchar(400),
+    leader     integer not null
+        constraint leader
+            references "user",
+    time_start timestamp,
+    time_end   timestamp
+);
+
+alter table day_crew
+    owner to postgres;
+
+create table day_crew_roster
+(
+    user_id integer
+        constraint date_crew_roaster_user_fkey
+            references "user",
+    crew_id integer
+        constraint date_crew_roaster_day_crew_fkey
+            references day_crew
+);
+
+alter table day_crew_roster
+    owner to postgres;
+
+create table calls
+(
+    id          serial
+        primary key,
+    description varchar(200),
+    summing_up  varchar(200),
+    address     varchar(45),
+    time_start  timestamp,
+    time_finish timestamp,
+    title       varchar(50)
+);
+
+alter table calls
+    owner to postgres;
+
+create table crew_calls
+(
+    call_id integer
+        references calls,
+    crew_id integer
+        references day_crew
+);
+
+alter table crew_calls
+    owner to postgres;
+
+create table permission
+(
+    id          serial
+        primary key,
+    action      boolean,
+    action_name varchar(50)
+);
+
+alter table permission
+    owner to postgres;
+
+create table ranks
+(
+    id   serial
+        primary key,
+    name varchar(20)
+);
+
+alter table ranks
+    owner to postgres;
+
+create table rank_permissions
+(
+    permission integer
+        references permission,
+    rank       integer
+        references ranks
+);
+
+alter table rank_permissions
+    owner to postgres;
+
+create table codes
+(
+    id     serial
+        primary key,
+    code   varchar(10),
+    userid integer
+        references "user"
+);
+
+alter table codes
+    owner to postgres;
+
+create table event_inventory
+(
+    eventid integer
+        references event,
+    itemid  integer
+        references inventory
+);
+
+alter table event_inventory
+    owner to postgres;
+
+create table users_ranks
+(
+    user_id integer
+        references "user",
+    rank_id integer
+        references ranks
+);
+
+alter table users_ranks
+    owner to postgres;
+
